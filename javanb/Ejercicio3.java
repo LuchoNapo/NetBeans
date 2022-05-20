b8bd99c379f76fdb6c053736f7a5f2a1ba69aa7c
Lucho Napo <Luciano_napo@hotmail.com>
2022-05-20 18:26:47 -0300

    Quinto Commit

--- javanb/Ejercicio3.java
+++ javanb/Ejercicio3.java
@@ -5,6 +5,8 @@
  */
 package javanb;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lucho Napo
@@ -15,7 +17,9 @@ public class Ejercicio3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        int queso = leer.nextInt();
+        
     }
     public static void Queso(){
         String quesoo = "Hola como estas";
