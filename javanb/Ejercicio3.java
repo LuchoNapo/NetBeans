aab7f408de2896786508c017a01ba645ebbd0534
Lucho Napo <Luciano_napo@hotmail.com>
2022-05-20 18:24:07 -0300

    Cuarto Commit

--- javanb/Ejercicio3.java
+++ javanb/Ejercicio3.java
@@ -17,5 +17,8 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public static void Queso(){
+        String quesoo = "Hola como estas";
+        
+    }
 }
